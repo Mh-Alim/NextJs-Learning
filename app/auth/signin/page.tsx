dcd5a06379f026a85c2901e8c2ae70906aa74a0a
Alim Khan <90334104+Mh-Alim@users.noreply.github.com>
2024-04-24 21:20:15 +0530

    just setup

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>this is auth/signin page</div>
+  )
+}
+
+export default page
+
+
---
+++ app/signup/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>this is sign up</div>;
+};
+
+export default page;
